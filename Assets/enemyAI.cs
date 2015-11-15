854bd75871d9f5e67d7fd505726f01a22e13cc49
davidalpha <david.alpha@gmail.com>
2015-11-15 14:23:44 +0100

    Spawn added
    + spawn
    + skeletEnemy

---
+++ Assets/enemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyAI : MonoBehaviour {
+
+	Transform Leader;
+	float speed = 5;
+	float maxDistance = 10;
+	float minDistance = 4;
+
+	void Start()	{
+		Leader = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update()	{
+		transform.LookAt (Leader);
+		Follow();
+	}
+	void Follow()	{
+		if (Vector3.Distance (transform.position, Leader.position) >= minDistance);
+		{
+			transform.position += transform.forward*speed*Time.deltaTime;
+		}
+	}
+
+}
