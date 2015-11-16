8d1ba6dfa760dab3fa3c59171c5a7ffb4162b47d
davidalpha <david.alpha@gmail.com>
2015-11-16 22:57:36 +0100

    Added Options to Spawn / Improved AI

--- Assets/TestScene ALSO FOR ALL TEST ASSETS/SpawnEnemy.cs
+++ Assets/TestScene ALSO FOR ALL TEST ASSETS/SpawnEnemy.cs
@@ -3,20 +3,44 @@ using System.Collections;
 
 public class SpawnEnemy : MonoBehaviour {
 
+	private int spawnTotal;
+
 	public GameObject spawnPoint;
 	public GameObject enemyPrefab;
+	public int spawnDelay;
+	public float spawnRate;
+	public int spawnAmount;
+
+
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("Spawn", 1, 1f); 
+		startSpawn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
+	void startSpawn(){
+		spawnTotal = 0;
+		InvokeRepeating ("Spawn", spawnDelay, spawnRate);
+	}
+
+
+	void stopSpawn(){
+		CancelInvoke ();
+		startSpawn ();
+	}
 
 	void Spawn(){
+
 		Rigidbody Enemy;
 		Enemy = Instantiate(enemyPrefab, spawnPoint.transform.position, transform.rotation) as Rigidbody;
+		spawnTotal++;
+		print (spawnTotal);
+		if (spawnTotal >= spawnAmount) {
+			stopSpawn();
+		}
+
 	}
 }
--- Assets/TestScene ALSO FOR ALL TEST ASSETS/enemyAI.cs
+++ Assets/TestScene ALSO FOR ALL TEST ASSETS/enemyAI.cs
@@ -3,35 +3,63 @@ using System.Collections;
 
 public class enemyAI : MonoBehaviour {
 
-	private Transform Leader;
+	private Transform Player;
+	private Transform Goal;
 	private Transform myTransform;
-	public float moveSpeed = 5;
+	private float moveSpeed;
 	public float maxDistance = 10;
 	public float minDistance = 1;
 	public float rotationSpeed = 2;
+	public float baseSpeed = 5;
+
 
 	void Awake()
 	{
+		moveSpeed = Random.Range(baseSpeed,(baseSpeed+0.5f));
 		myTransform = transform; //cache transform data for easy access/preformance
 	}
 
 	void Start()	{
-		Leader = GameObject.FindGameObjectWithTag ("Player").transform;
+		Player = GameObject.FindGameObjectWithTag ("Player").transform;
+		Goal = GameObject.FindGameObjectWithTag ("Goal").transform;
 	}
 
 	void Update()	{
-		transform.LookAt (Leader);
-		Follow();
+	
+	if (Vector3.Distance (transform.position, Player.position) >= maxDistance) {
+			transform.LookAt (Goal);
+			FollowGoal();
+		} else {
+
+			transform.LookAt (Player);
+			FollowPlayer();
+		}
+
 	}
-	void Follow()	{
-		if (Vector3.Distance (transform.position, Leader.position) >= minDistance)
-		{
+	void FollowPlayer()	{
+
+		if (Vector3.Distance (transform.position, Player.position) >= minDistance) {
 			
-			//move towards the player
-			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-			Quaternion.LookRotation(Leader.position - myTransform.position), rotationSpeed*Time.deltaTime);
+			//move towards the Player
+
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation,
+			Quaternion.LookRotation (Player.position - myTransform.position), rotationSpeed * Time.deltaTime);
 			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
 
+		} else {
+			Destroy (gameObject);
+		}
+	}
+	// 2 functies omdat in de toekomst mogelijk ander gedrag moet worden toegevoegd
+	void FollowGoal()	{
+		if (Vector3.Distance (transform.position, Goal.position) >= minDistance)
+		{
+			
+			//move towards the Goal
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+			Quaternion.LookRotation(Goal.position - myTransform.position), rotationSpeed*Time.deltaTime);
+			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+			
 		} else {
 			Destroy(gameObject);
 		}
